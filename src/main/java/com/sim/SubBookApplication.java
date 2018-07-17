508191cf8fd98c74db0300c6b5140a1709c38f57
huangyujiao <huangyujiao@zjaisino.com>
2018-07-17 09:45:04 +0800

    文件提交

---
+++ src/main/java/com/sim/SubBookApplication.java
@@ -0,0 +1,12 @@
+package com.sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubBookApplication.class, args);
+	}
+}
